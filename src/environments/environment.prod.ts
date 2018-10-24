77b0941c9e6d6fe8c35d318f592e74d758f9183e
martinboykov <martinboykov@mail.bg>
2018-10-24 14:41:21 +0300

    env remove

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBodMeAJSXgUgLUK8REdusq3OTtCAsq4fE',
-    authDomain: 'github-followers-app-aee0b.firebaseapp.com',
-    databaseURL: 'https://github-followers-app-aee0b.firebaseio.com',
-    projectId: 'github-followers-app-aee0b',
-    storageBucket: 'github-followers-app-aee0b.appspot.com',
-    messagingSenderId: '806117470172'
+    apiKey: '....................................',
+    authDomain: '....................................',
+    databaseURL: '....................................',
+    projectId: '....................................',
+    storageBucket: '....................................',
+    messagingSenderId: '....................................',
   }
 };
